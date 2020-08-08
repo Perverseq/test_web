231e7ba0e9beb2cb4c0e71c242f24040e8bf76cf
Perverseq <luchniknya@gmail.com>
2020-08-08 14:47:18 +0500

    Исправил ошибку в реализации шага "нажать на"

--- steps/test.py
+++ steps/test.py
@@ -1,6 +1,5 @@
 from behave import *
 from lib.pages import *
-TIMEOUT = 60
 
 
 @when('зайти на сайт "{site}"')
@@ -17,19 +16,8 @@ def step_impl(context, element_name, text):
 
 @then('нажать на "{element_name}" "{text}"')
 def step_impl(context, element_name, text):
-    articles = {'Россия': '//*[@href="/equities/russia"]',
-                "Вход": "//*[@class='login bold']"}
-    buttons = {"Вход": "//*[@class='newButton orange'][text()='Вход']",
-               "маленького всплывающего окна": "//*[@class='popupCloseIcon']"}
-    pathes = {'пункт': articles,
-              'кнопку': buttons,
-              'крестик': buttons}
-    element = WebDriverWait(context.browser, TIMEOUT).until(
-        EC.presence_of_element_located((By.XPATH, pathes[element_name][text])))
-    element.click()
-    #context.page = MainPage(context)
-    #context.page.press(context, element_name, text)
-# TODO: разобраться с ошибкой в PageObject
+    context.page = MainPage(context)
+    context.page.press_element(context, element_name, text)
 
 
 @then('проверить "{element_name}" "{value}"')
