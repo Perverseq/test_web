d74183adf7e73cdf88cc67f42886e3b236982e79
Perverseq <luchniknya@gmail.com>
2020-08-12 20:16:02 +0500

    Добавил страницу для отображения доходности

--- app.py
+++ app.py
@@ -12,5 +12,12 @@ def index():
     return render_template('index.html', articles=articles)
 
 
+@app.route('/dividends')
+def dividends():
+    with open(".\\dividends.json", 'r', encoding='utf-8') as outfile:
+        articles = list(json.load(outfile).items())
+    return render_template('dividends.html', articles=articles)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
