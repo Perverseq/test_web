7380285249d589a4ae15493b91e943b5e4e60c2e
Perverseq <luchniknya@gmail.com>
2020-10-03 20:00:07 +0500

    Добавил функцию, убрал повторения

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template, url_for
 import json
 
+
+def render_articles(file, page):
+    with open(file, 'r', encoding='utf-8') as outfile:
+        articles = list(json.load(outfile).items())
+    return render_template(page, articles=articles)
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/home')
 def index():
-    with open(".\\result.json", 'r', encoding='utf-8') as outfile:
-        articles = list(json.load(outfile).items())
-    return render_template('index.html', articles=articles)
+    return render_articles(".\\result.json", 'index.html')
 
 
 @app.route('/dividends')
 def dividends():
-    with open(".\\dividends.json", 'r', encoding='utf-8') as outfile:
-        articles = list(json.load(outfile).items())
-    return render_template('dividends.html', articles=articles)
+    return render_articles(".\\dividends.json", 'dividends.html')
 
 
 if __name__ == '__main__':
